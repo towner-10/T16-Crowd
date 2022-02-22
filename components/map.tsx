5b2481e4cf608cdc644b68443eedb6b6fa674773
Collin Town <collintown@gmail.com>
2022-02-21 22:14:06 -0500

    Working dashboard alongside API - Added Active Queries sidebar - Added Top Tweets sidebar - Added points on map - Added popups to points - Updated sizing based on score - Interfaces for data models from API

--- components/map.tsx
+++ components/map.tsx
@@ -1,21 +1,23 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Map, { HeatmapLayer, CircleLayer, Layer, Marker, Source } from 'react-map-gl';
+import Map, { HeatmapLayer, CircleLayer, Layer, Popup, Source, MapLayerMouseEvent } from 'react-map-gl';
 import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
+import Link from 'next/link';
+
+interface ITweetPoint {
+	longitude: any;
+	latitude: any;
+	score: number;
+	id: string;
+}
 
 const heatmapLayerStyle: HeatmapLayer = {
 	id: 'heatmap-heat',
 	type: 'heatmap',
 	maxzoom: 9,
 	paint: {
-		// Increase the heatmap weight based on frequency and property magnitude
-		'heatmap-weight': ['interpolate', ['linear'], ['get', 'mag'], 0, 0, 6, 1],
-		// Increase the heatmap color weight weight by zoom level
-		// heatmap-intensity is a multiplier on top of heatmap-weight
+		'heatmap-weight': ['interpolate', ['linear'], ['get', 'score'], 0, 0, 6, 1],
 		'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 10, 3],
-		// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-		// Begin color ramp at 0-stop with a 0-transparancy color
-		// to create a blur-like effect.
 		'heatmap-color': [
 			'interpolate',
 			['linear'],
@@ -33,52 +35,50 @@ const heatmapLayerStyle: HeatmapLayer = {
 			0.9,
 			'rgb(255,201,101)'
 		],
-		// Adjust the heatmap radius by zoom level
 		'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 10, 20],
-		// Transition from heatmap to circle layer by zoom level
 		'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 7, 1, 9, 0]
 	}
 };
 
 const pointLayerStyle: CircleLayer = {
-	id: 'earthquakes-point',
+	id: 'tweet-point',
 	type: 'circle',
-	source: 'earthquakes',
+	source: 'tweets',
 	minzoom: 7,
 	paint: {
-		// Size circle radius by earthquake magnitude and zoom level
 		'circle-radius': [
 			'interpolate',
 			['linear'],
 			['zoom'],
-			7,
-			['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
-			16,
-			['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+			7, ['interpolate', 
+				['linear'], 
+				['get', 'score'], 
+				10, 1, 
+				100, 4,
+				1000, 8
+			],
+			16, ['interpolate', 
+				['linear'], 
+				['get', 'score'], 
+				10, 1, 
+				100, 50,
+				4000, 200
+			]
 		],
-		// Color circle by earthquake magnitude
-		'circle-color': [
-			'interpolate',
+		'circle-color': ['interpolate',
 			['linear'],
-			['get', 'mag'],
-			1,
-			'rgba(33,102,172,0)',
-			2,
-			'rgb(103,169,207)',
-			3,
-			'rgb(209,229,240)',
-			4,
-			'rgb(253,219,199)',
-			5,
-			'rgb(239,138,98)',
-			6,
-			'rgb(178,24,43)'
+			['get', 'score'],
+			10, 'rgba(33,102,172,0)',
+			20, 'rgb(103,169,207)',
+			30, 'rgb(209,229,240)',
+			40, 'rgb(253,219,199)',
+			50, 'rgb(239,138,98)',
+			500, 'rgb(178,24,43)',
+			1000, 'rgb(255,201,101)'
 		],
 		'circle-stroke-color': 'white',
 		'circle-stroke-width': 1,
-		// Transition from heatmap to circle layer by zoom level
-		'circle-opacity': [
-			'interpolate',
+		'circle-opacity': ['interpolate',
 			['linear'],
 			['zoom'],
 			7,
@@ -92,11 +92,15 @@ const pointLayerStyle: CircleLayer = {
 
 function DashboardMap() {
 
+	const [popupInfo, setPopupInfo] = useState<ITweetPoint | undefined>(undefined);
 	const [geojson, setGeoJson] = useState(null);
 
 	useEffect(() => {
-		axios('https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson').then(res => {
-			setGeoJson(res.data);
+		axios('http://127.0.0.1:5000/queries/active/list/geojson').then(res => {
+			if (res.data.status === 200) {
+				setGeoJson(res?.data['geojson']);
+			}
+			else setGeoJson(null);
 		}).catch(err => {
 			console.log(err);
 		});
@@ -109,18 +113,47 @@ function DashboardMap() {
 	return (
 		<Map reuseMaps
 			initialViewState={{
-				longitude: -122.4,
-				latitude: 37.8,
-				zoom: 14
+				longitude: -79.347,
+				latitude: 43.651,
+				zoom: 5
 			}}
+			interactiveLayerIds={['tweet-point']}
 			mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
 			style={{ width: '100%', height: '90vh' }}
-			mapStyle="mapbox://styles/mapbox/dark-v10">
+			mapStyle="mapbox://styles/mapbox/dark-v10"
+			onClick={(event) => {
+				const { features } = event;
+				if (features !== undefined && features.length > 0) {
+					if (features[0].geometry.type === 'Point' && features[0].properties !== null) {
+						const tweet: ITweetPoint = {
+							longitude: features[0].geometry.coordinates[0],
+							latitude: features[0].geometry.coordinates[1],
+							score: features[0].properties.score,
+							id: features[0].properties.id
+						};
+
+						setPopupInfo(tweet);
+					}
+					else setPopupInfo(undefined);
+				}
+				else setPopupInfo(undefined);
+			}}>
 			{data && (
 				<Source id="heatmap" type="geojson" data={data}>
 					<Layer {...heatmapLayerStyle} />
 					<Layer {...pointLayerStyle} />
 				</Source>
+			)}
+			{popupInfo && (
+				<Popup
+					longitude={Number(popupInfo.longitude)} 
+					latitude={Number(popupInfo.latitude)}
+					anchor="bottom"
+					closeOnClick={true}
+					onClose={() => setPopupInfo(undefined)}>
+					The algo score is: {popupInfo.score.toFixed(2)}<br/>
+					<a target='_blank' href={`https://twitter.com/anyuser/status/${popupInfo.id}`} rel='noopener noreferrer' className='text-blue-400 hover:text-blue-300'>Click here</a> to see the tweet.
+				</Popup>
 			)}
 		</Map>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,159 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-
 import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { format, parseISO } from 'date-fns'
+
+// Disable server side rendering
 const Map = dynamic(
   () => {
     return import('../components/map')
   }, {ssr: false});
 
-const Home: NextPage = () => {
+interface IQuery {
+  id: string;
+	name: string;
+  location: any;
+  startDate: Date;
+  endDate: Date;
+  keywords: string[];
+  frequency: number;
+	maxTweets: number;
+}
+
+interface ITweet {
+  id: string;
+  queryID: string;
+  likes: number;
+  retweets: number;
+  replies: number;
+  content: string;
+  media: any[];
+  location: any;
+  createdAt: Date;
+  keywordCount: number;
+  interactionScore: number;
+  relatabilityScore: number;
+}
+
+// Image recognition
+// Use Google Image Recognition API
+// Assign data points from events to members
+// One account
+// Microsoft image recognition API
+
+const Dashboard: NextPage = () => {
+
+  let [queries, setQueries] = useState<IQuery[] | undefined>(undefined);
+  let [tweets, setTweets] = useState<ITweet[] | undefined>(undefined);
+
+  useEffect(() => {
+    axios('http://127.0.0.1:5000/queries/active/list').then(res => {
+			if (res.data.status === 200) {
+        let queriesList: IQuery[] = [];
+        for (let query of res.data['queries']) {
+          queriesList.push({
+            id: query['_id'],
+            name: query['name'],
+            location: query['location'],
+            startDate: parseISO(query['startDate']),
+            endDate: parseISO(query['endDate']),
+            keywords: query['keywords'],
+            frequency: query['frequency'],
+            maxTweets: query['maxTweets']
+          });
+        }
+				setQueries(queriesList);
+			}
+			else setQueries(undefined);
+		}).catch(err => {
+			console.log(err);
+		});
+
+    axios('http://127.0.0.1:5000/queries/active/list/tweets?limit=5').then(res => {
+			if (res.data.status === 200) {
+        let tweetsList: ITweet[] = [];
+        for (let query of res.data['tweets']) {
+          console.log(query);
+          tweetsList.push({
+            id: query['id'],
+            queryID: query['qId'],
+            likes: query['likes'],
+            retweets: query['rt'],
+            replies: query['rp'],
+            media: query['media'],
+            createdAt: parseISO(query['date']),
+            location: query['loc'],
+            content: query['content'],
+            keywordCount: query['kc'],
+            interactionScore: query['is'],
+            relatabilityScore: query['rs']
+          });
+        }
+				setTweets(tweetsList);
+			}
+			else setQueries(undefined);
+		}).catch(err => {
+			console.log(err);
+		});
+  }, []);
+
   return (
-    <div className="p-6">
+    <div className="h-screen bg-white dark:bg-gray-700">
       <Head>
         <title>T16 - Dashboard</title>
         <meta name="description" content="Dashboard for NTP use" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="grid overflow-hidden grid-cols-4 grid-rows-5 gap-2">
+      <main className="p-2 grid overflow-hidden grid-cols-4 grid-rows-5 gap-2">
         <div className="row-span-5 col-span-3">
-          <h1 className="font-bold text-xl md:text-2xl lg:text-5xl">Dashboard</h1>
-          <div className='mt-2'>
+          <h1 className="font-bold text-xl md:text-xl xl:text-4xl dark:text-white">Dashboard</h1>
+          <div className='mt-2 rounded-md'>
             <Map></Map>
           </div>
         </div>
-        <div className="row-span-2 min-w-full min-h-full h-32">
-          <h1 className="m-2 font-bold text-xl md:text-2xl lg:text-5xl">Current Missions</h1>
+        <div className="row-span-2 min-w-full min-h-full overflow-hidden">
+          <h1 className="m-2 font-bold text-xl md:text-xl xl:text-4xl dark:text-white">Active Queries</h1>
+          <div className='overflow-y-auto' style={{height: '30vh'}}>
+            {queries && queries.map((query, index) => {
+              return(
+                <a key={index} href='#' className="block m-3 p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{query.name}</h5>
+                  <h5 className="mb-2 text-l tracking-tight text-gray-900 dark:text-gray-200">{format(query.startDate, 'yyyy/MM/dd')} to {format(query.endDate, 'yyyy/MM/dd')}</h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Keywords: </span>{query.keywords.toLocaleString().replaceAll(',', ', ')}</p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Frequency: </span><span className='italic'>{query.frequency}</span> min</p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Max Tweets: </span><span className='italic'>{query.maxTweets}</span></p>
+                </a>
+              );
+            })}
+          </div>
         </div>
-        <div className="row-span-2 min-w-full min-h-full h-32">
-          <h1 className="m-2 font-bold text-xl md:text-2xl lg:text-5xl">Past Missions</h1>
+        <div className="row-span-2 min-w-full min-h-full">
+          <h1 className="m-2 font-bold text-xl md:text-xl xl:text-4xl dark:text-white">Top 5 Tweets</h1>
+          <div className='overflow-y-auto' style={{height: '30vh'}}>
+            {tweets && tweets.map((tweet, index) => {
+              return(
+                <div key={index} className="block m-3 p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white break-words">{tweet.id}</h5>
+                  <h5 className="mb-2 text-l tracking-tight text-gray-900 dark:text-gray-200">{format(tweet.createdAt, 'yyyy/MM/dd')} - {tweet.relatabilityScore.toFixed(2)} score</h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Likes: </span>{tweet.likes}</p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Matches: </span><span className='italic'>{tweet.keywordCount}</span> keywords matches</p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Media: </span><span className='italic'>{tweet.media.length}</span> pieces of content</p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400"><span className='font-bold'>Link: </span><span className='italic'><a target='_blank' href={`https://twitter.com/anyuser/status/${tweet.id}`} rel='noopener noreferrer' className='text-blue-400 hover:text-blue-300'>Twitter</a></span></p>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div className="min-w-full min-h-full h-32 p-2 flex flex-col justify-center items-center">
+        <div className="min-w-full min-h-full p-2 flex flex-col justify-center items-center">
           <Link href="/login" passHref>
-            <button className="w-full px-3 py-2 my-2 border bg-purple-500 hover:bg-purple-700 rounded-md shadow-sm text-sm leading-5 text-white font-semibold">Login</button>
+            <button className="w-full px-3 py-2 my-2 rounded-md border-2 border-purple-700 bg-purple-600 hover:bg-purple-500 shadow-sm text-sm leading-5 font-semibold text-white">Login</button>
           </Link>
           <Link href="/register" passHref>
-            <button className="w-full px-3 py-2 my-2 border rounded-md hover:bg-slate-100 shadow-sm text-sm leading-5 font-semibold">Register</button>
+            <button className="w-full px-3 py-2 my-2 border rounded-md hover:bg-slate-100 dark:hover:bg-slate-500 shadow-sm text-sm leading-5 font-semibold dark:text-white">Register</button>
           </Link>
         </div>
       </main>
@@ -43,4 +161,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Dashboard
